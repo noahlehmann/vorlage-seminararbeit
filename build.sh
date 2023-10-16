ca8314c96c39142b17c3564c5a9732bcd09a172c
nlehmann <noah.lehmann@protonmail.com>
2023-10-16 13:37:41 +0200

    main: latex build fix

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 find . \( -iname '*.out' -o -iname '*.toc' -o  -iname '*.log' -o -iname '*.aux' -o -iname '*.dvi' -o -iname '*.lol' -o -iname '*.lof' -o -iname '*.run.xml' -o -iname '*.fdb_latexmk' -o -iname '*.fls' -o -iname '*.bbl' -o -iname '*.bcf' -o -iname '*.blg' -o -iname '*.lot' \) -exec rm -R {} +
-docker run --rm -t --user="$(id -u):$(id -g)" --net=none -v "$(pwd):/tmp" leplusorg/latex:1.0 latexmk -outdir=/tmp -pdf /tmp/seminararbeit.tex
-docker run --rm -t --user="$(id -u):$(id -g)" --net=none -v "$(pwd):/tmp" leplusorg/latex:1.0 latexmk -outdir=/tmp -pdf /tmp/seminararbeit.tex
+docker run --rm -t --platform linux/amd64 --user=root --net=none -v "$(pwd):/tmp" leplusorg/latex latexmk -outdir=/tmp -pdf /tmp/seminararbeit.tex
+docker run --rm -t --platform linux/amd64 --user=root --net=none -v "$(pwd):/tmp" leplusorg/latex latexmk -outdir=/tmp -pdf /tmp/seminararbeit.tex
 find . \( -iname '*.out' -o -iname '*.toc' -o  -iname '*.log' -o -iname '*.aux' -o -iname '*.dvi' -o -iname '*.lol' -o -iname '*.lof' -o -iname '*.run.xml' -o -iname '*.fdb_latexmk' -o -iname '*.fls' -o -iname '*.bbl' -o -iname '*.bcf' -o -iname '*.blg' -o -iname '*.lot' \) -exec rm -R {} +
